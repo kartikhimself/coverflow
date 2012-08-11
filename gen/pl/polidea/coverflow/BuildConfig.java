cb5ac3051908a0f5e083064cb2c49d9f81385c42
Kartik Sharma <kshark27@hotmail.com>
2012-08-11 18:42:29 +0530

    cooverflow source
    added coverflow widget source

---
+++ gen/pl/polidea/coverflow/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pl.polidea.coverflow;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/pl/polidea/coverflow/testingactivity/CoverFlowTestingActivity.java
@@ -0,0 +1,92 @@
+package pl.polidea.coverflow.testingactivity;
+
+import pl.polidea.coverflow.CoverFlow;
+import pl.polidea.coverflow.R;
+import pl.polidea.coverflow.ReflectingImageAdapter;
+import pl.polidea.coverflow.ResourceImageAdapter;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * The Class CoverFlowTestingActivity.
+ */
+public class CoverFlowTestingActivity extends Activity {
+
+    private TextView textView;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        textView = (TextView) findViewById(this.getResources()
+                .getIdentifier("statusText", "id", "pl.polidea.coverflow"));
+        // note resources below are taken using getIdentifier to allow importing
+        // this library as library.
+        final CoverFlow coverFlow1 = (CoverFlow) findViewById(this.getResources().getIdentifier("coverflow", "id",
+                "pl.polidea.coverflow"));
+        setupCoverFlow(coverFlow1, false);
+        final CoverFlow reflectingCoverFlow = (CoverFlow) findViewById(this.getResources().getIdentifier(
+                "coverflowReflect", "id", "pl.polidea.coverflow"));
+        setupCoverFlow(reflectingCoverFlow, true);
+    }
+
+    /**
+     * Setup cover flow.
+     * 
+     * @param mCoverFlow
+     *            the m cover flow
+     * @param reflect
+     *            the reflect
+     */
+    private void setupCoverFlow(final CoverFlow mCoverFlow, final boolean reflect) {
+        BaseAdapter coverImageAdapter;
+        if (reflect) {
+            coverImageAdapter = new ReflectingImageAdapter(new ResourceImageAdapter(this));
+        } else {
+            coverImageAdapter = new ResourceImageAdapter(this);
+        }
+        mCoverFlow.setAdapter(coverImageAdapter);
+        mCoverFlow.setSelection(2, true);
+        setupListeners(mCoverFlow);
+    }
+
+    /**
+     * Sets the up listeners.
+     * 
+     * @param mCoverFlow
+     *            the new up listeners
+     */
+    private void setupListeners(final CoverFlow mCoverFlow) {
+        mCoverFlow.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(final AdapterView< ? > parent, final View view, final int position, final long id) {
+                textView.setText("Item clicked! : " + id);
+            }
+
+        });
+        mCoverFlow.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(final AdapterView< ? > parent, final View view, final int position, final long id) {
+                textView.setText("Item selected! : " + id);
+            }
+
+            @Override
+            public void onNothingSelected(final AdapterView< ? > parent) {
+                textView.setText("Nothing clicked!");
+            }
+        });
+    }
+
+}
